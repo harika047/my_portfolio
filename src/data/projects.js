27062e80e1d2bc0468d0da23df1bb180604bb862
harika047 <harikahoney2005@gmail.com>
2026-06-12 11:22:51 +0530

    Enhanced projects section

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "ML web application predicting customer purchase intent with 85% accuracy.",
     tech: ["Python", "Flask", "Scikit Learn"],
-    github: "#",
+    github: "https://github.com/harika047/customer-purchase-prediction-app.git",
     demo: "https://customer-purchase-prediction-app.onrender.com"
   },
   {
@@ -14,7 +14,7 @@ export const projects = [
     description:
       "Responsive coffee ordering platform with cart functionality.",
     tech: ["HTML", "CSS", "JavaScript", "Bootstrap"],
-    github: "#",
+    github: "https://github.com/harika047/Buzz-and-Bites-App.git",
     demo: "https://harika047.github.io/Buzz-and-Bites-App/"
   }
 ];
